e6e9d65d59447e2e1d7432fcebe815a8c68bb7c3
laperry1 <laperry7706@gmail.com>
2017-11-25 01:54:48 -0500

    Fix build (DERP): error: static declaration of 'sysrq_reset_downtime_ms' follows non-static declaration
    	modified:   drivers/tty/sysrq.c

--- drivers/tty/sysrq.c
+++ drivers/tty/sysrq.c
@@ -55,9 +55,6 @@
 static int __read_mostly sysrq_enabled = CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE;
 static bool __read_mostly sysrq_always_enabled;
 
-unsigned short platform_sysrq_reset_seq[] __weak = { KEY_RESERVED };
-int sysrq_reset_downtime_ms __weak;
-
 bool sysrq_on(void)
 {
 	return sysrq_enabled || sysrq_always_enabled;
